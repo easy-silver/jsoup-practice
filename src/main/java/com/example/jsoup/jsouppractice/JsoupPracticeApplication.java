85505e6fe67d4b1dae7d5ee7330613617192ef90
Timo <developer.timo@gmail.com>
2021-10-16 14:54:33 +0900

    Initial commit

---
+++ src/main/java/com/example/jsoup/jsouppractice/JsoupPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoup.jsouppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoupPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsoup/jsouppractice/JsoupPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoup.jsouppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoupPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
